1f8850cc6b3c5ba0bc9d80895e79b52fdeb6ed3d
Paul Solt <PaulSolt@gmail.com>
2019-09-02 11:26:10 -0400

    Working prototype

--- TopRatedReviews/StarRatings.swift
+++ TopRatedReviews/StarRatings.swift
@@ -25,58 +25,73 @@ struct StarRow: View {
 }
 
 struct StarRatings: View {
-    @State private var size: CGSize = CGSize(width: 8, height: 8)
-    @State var stars: [Int] = [5, 50, 3, 1, 3] {
+    //    init(stars: [Int]) {
+    //        self.stars = [ 34, 4, 4, 5, 5]
+    //    }
+    
+    var stars: [Int] = [100, 50, 3, 1, 3] {
         mutating didSet {
-            numStars = stars.reduce(into: 0, { (result, next) in
-                result += next
-            })
-            print("Stars: \(numStars)")
+            calculateLengths()
         }
     }
-//    private var starArray: [Int] = [5, 4, 3, 2, 1]
+    //    private var starArray: [Int] = [5, 4, 3, 2, 1]
+    private var size: CGSize = CGSize(width: 8, height: 8)
     
-    private var numStars: Int = 0
+    private var numStars: Int = 100
+    
+    init() {
+        print("HI")
+        
+        
+        // total stars - stars
+        
+        calculateLengths()
+    }
+    
+    private mutating func calculateLengths() {
+        numStars = stars.reduce(0, +)
+        
+        lengths = []
+        for i in 0..<5 {
+            let rating: CGFloat = CGFloat(stars[i]) / CGFloat(numStars)
+            lengths.append(rating)
+        }
+    }
+    
+    private var lengths: [CGFloat] = []
     
     var body: some View {
         
         HStack(alignment: .bottom) {
             
             VStack(alignment: .trailing, spacing: 2) {
-                ForEach((1...5), id: \.self) { index in
-                    StarRow(count: 6 - index)
+                ForEach((1...5).reversed(), id: \.self) { index in
+                    StarRow(count: index)
                 }
             }
             
             VStack(alignment: .leading, spacing: 2) {
-                ForEach((1...5), id: \.self) { index in
-//                    Text("\(self.stars[index - 1])")
-                    Rectangle()
-                        .frame(width: CGFloat(self.stars[index - 1]), height: self.size.height)
-                        .foregroundColor(Color(white: 0.7))
-                    
-                    
-//                    HStack(alignment: .center, spacing: 0) {
-//                        ForEach((0...5), id: \.self) { numStars
-//                            Image(systemName: "star.fill")
-//                                .resizable()
-//                                .frame(width: 8, height: 8)
-//                                .foregroundColor(.red)
-//                            Image(systemName: "star.fill")
-//                                .resizable()
-//                                .frame(width: 8, height: 8)
-//                                .foregroundColor(.red)
-//
-//
-//
-//                        }
-//                    }
+                ForEach((0..<5), id: \.self) { index in
+                    HStack {
+                        //                        Text("\(self.stars[index])")
+                        ZStack(alignment: .leading) { //alignment: .center, spacing: 0) {
+                            Rectangle()
+                                .frame(width: CGFloat(100), height: self.size.height)
+                                .foregroundColor(Color(white: 0.4))
+                            Rectangle()
+                                .frame(width: CGFloat(self.lengths[index]) * 100, height: self.size.height)
+                                .foregroundColor(Color(white: 0.8))
+                            
+                        }
+                    }
                 }
                 
             }
         }.background(
             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .foregroundColor(Color(white: 0.9)) // red: 0.3, green: 0.3, blue: 0.3)))
+                .padding(-10)
+                .foregroundColor(Color(white: 0.2)) // red: 0.3, green: 0.3, blue: 0.3)))
+            
         )
         
     }
@@ -84,7 +99,10 @@ struct StarRatings: View {
 
 struct StarRatings_Previews: PreviewProvider {
     static var previews: some View {
-        StarRatings()
+        
+        StarRatings() // stars: [100, 34, 3, 4, 2])
+        //        StarRatings(
+        //        StarRatings(stars: [100, 34, 3, 3, 5])
         //        .environmentObject([])
     }
 }
