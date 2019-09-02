d9cb71ea404db7e4230c5f98d41277d290c24592
Paul Solt <PaulSolt@gmail.com>
2019-09-02 18:15:05 -0400

    Cleaned up code for demo

--- TopRatedReviews/StarRatings.swift
+++ TopRatedReviews/StarRatings.swift
@@ -25,71 +25,89 @@ struct StarRow: View {
 }
 
 struct StarRatings: View {
-    //    init(stars: [Int]) {
-    //        self.stars = [ 34, 4, 4, 5, 5]
-    //    }
     
     var stars: [Int] = [100, 50, 3, 1, 3] {
         mutating didSet {
             calculateLengths()
         }
     }
-    //    private var starArray: [Int] = [5, 4, 3, 2, 1]
+    
     private var size: CGSize = CGSize(width: 8, height: 8)
     
-    private var numStars: Int = 100
+    private var averageRating: Double = 5
     
     init() {
-        print("HI")
-        
-        
-        // total stars - stars
-        
         calculateLengths()
     }
     
     private mutating func calculateLengths() {
-        numStars = stars.reduce(0, +)
+        totalStars = stars.reduce(0, +)
         
         lengths = []
         for i in 0..<5 {
-            let rating: CGFloat = CGFloat(stars[i]) / CGFloat(numStars)
+            let rating: CGFloat = CGFloat(stars[i]) / CGFloat(totalStars)
             lengths.append(rating)
         }
+        
+        averageRating = 0.0
+        for i in 0..<5 {
+            //            let rating = stars[i]
+            averageRating += (Double(stars[i]) * Double(5 - i))
+        }
+        averageRating /= Double(totalStars)
     }
+    
+    private var totalStars = 0
+    
+    private var ratingFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 1
+        return formatter
+    }()
     
     private var lengths: [CGFloat] = []
     
     var body: some View {
         
-        HStack(alignment: .bottom) {
-            
+        HStack(alignment: .top) {
+            VStack(alignment: .leading) {
+                Text(ratingFormatter.string(from: NSNumber(value: averageRating)) ?? "INVALID") // TODO: should we do something else for nil values?
+                    .font(.largeTitle)
+                    .foregroundColor(Color(white: 0.8))
+                Text("out of 5")
+                    .foregroundColor(Color(white: 0.5))
+            }
             VStack(alignment: .trailing, spacing: 2) {
                 ForEach((1...5).reversed(), id: \.self) { index in
                     StarRow(count: index)
                 }
             }
             
-            VStack(alignment: .leading, spacing: 2) {
-                ForEach((0..<5), id: \.self) { index in
-                    HStack {
-                        //                        Text("\(self.stars[index])")
-                        ZStack(alignment: .leading) { //alignment: .center, spacing: 0) {
-                            Rectangle()
-                                .frame(width: CGFloat(100), height: self.size.height)
-                                .foregroundColor(Color(white: 0.4))
-                            Rectangle()
-                                .frame(width: CGFloat(self.lengths[index]) * 100, height: self.size.height)
-                                .foregroundColor(Color(white: 0.8))
-                            
+            VStack(alignment: .trailing, spacing: 4) {
+                VStack(alignment: .leading, spacing: 2) {
+                    ForEach((0..<5), id: \.self) { index in
+                        HStack {
+                            //                        Text("\(self.stars[index])")
+                            ZStack(alignment: .leading) {
+                                Rectangle()
+                                    .frame(width: CGFloat(100), height: self.size.height)
+                                    .foregroundColor(Color(white: 0.4))
+                                Rectangle()
+                                    .frame(width: CGFloat(self.lengths[index]) * 100, height: self.size.height)
+                                    .foregroundColor(Color(white: 0.8))
+                                
+                            }
                         }
                     }
                 }
-                
+                Text("\(totalStars) Ratings")
+                    .foregroundColor(Color(white: 0.8))
             }
+            
         }.background(
             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .padding(-10)
+                .padding(-20)
                 .foregroundColor(Color(white: 0.2)) // red: 0.3, green: 0.3, blue: 0.3)))
             
         )
