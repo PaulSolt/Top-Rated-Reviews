f2a9c7151604be2adffcb738dd9d0858197ff028
Paul Solt <PaulSolt@gmail.com>
2019-09-08 12:31:38 -0400

    Updated project

---
+++ TopRatedReviews/AppReviewStore.swift
@@ -0,0 +1,37 @@
+//
+//  AppReviewStore.swift
+//  TopRatedReviews
+//
+//  Created by Paul Solt on 9/8/19.
+//  Copyright © 2019 Paul Solt. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class AppReviewStore: ObservableObject {
+    @Published private(set) var reviews: [Review] = []
+    
+    private let service: AppReviewService
+    
+    init(service: AppReviewService) {
+        self.service = service
+    }
+    
+    func fetchReviews(for appId: String, ordering: AppReviewURL.ReviewOrdering) {
+        
+        let appURL = AppReviewURL(appId: appId, ordering: ordering)
+        
+        service.fetchReviews(for: appURL) { result in  // TODO: need [weak self]?
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let reviews):
+                    self.reviews = reviews
+                case .failure(let error):
+                    print("Error fetching reviews: \(error)")
+                    self.reviews = []
+                }
+            }
+        }
+    }
+}
--- TopRatedReviews/ReviewList.swift
+++ TopRatedReviews/ReviewList.swift
@@ -8,33 +8,6 @@
 
 import SwiftUI
 
-class AppReviewStore: ObservableObject {
-    @Published private(set) var reviews: [Review] = []
-    
-    private let service: AppReviewService
-    
-    init(service: AppReviewService) {
-        self.service = service
-    }
-    
-    func fetchReviews(for appId: String, ordering: AppReviewURL.ReviewOrdering) {
-        
-        let appURL = AppReviewURL(appId: appId, ordering: ordering)
-        
-        service.fetchReviews(for: appURL) { result in  // TODO: need [weak self]?
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let reviews):
-                    self.reviews = reviews
-                case .failure(let error):
-                    print("Error fetching reviews: \(error)")
-                    self.reviews = []
-                }
-            }
-        }
-    }
-}
-
 struct ReviewList: View {
 //    var reviews: [Review] = []  // Prototype
     @EnvironmentObject var reviewStore: AppReviewStore
